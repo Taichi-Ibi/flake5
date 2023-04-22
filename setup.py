6d50423e8877804268026613558459ca8f9a23f3
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-22 16:56:25 +0900

    upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flake5",
-    version="0.0.1",
+    version="0.0.2",
     author="t.ibi",
     author_email="t.ibi@estyle-inc.jp",
     description="search unnecessary imports in ipynb file",
@@ -13,6 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Taichi-Ibi/flake5",
     packages=setuptools.find_packages(),
-    entry_points={"console_scripts": ["flake5 = flake5.flake5:flake5"]},
+    entry_points={"console_scripts": ["flake5 = flake5:flake5"]},
     python_requires=">=3.8",
 )
