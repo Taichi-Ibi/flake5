086c42105559eede1060126b0b73862aee942131
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-04-26 09:26:03 +0900

    [mod] 空セル対応

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flake5",
-    version="0.0.9",
+    version="0.0.91",
     author="t.ibi",
     author_email="t.ibi@estyle-inc.jp",
     description="search unnecessary imports in ipynb file",
